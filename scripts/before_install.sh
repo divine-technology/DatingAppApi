4bc0823dd064ac5dfdcdb055f8be4e57b381e93b
Faruk Sinanovic <fsinanovic1@gmail.com>
2023-09-29 18:49:38 +0200

    Skripte za Deployment i Yaml

---
+++ scripts/before_install.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# node i npm download
+curl -o https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash .~/.nvm/nvm.sh
+nvm install node
+
+#kreiranje foldera
+DIR="home/ec2-user/DatingApp"
+if [ -d "${DIR}" ]; then
+   echo "${DIR} Postoji"
+else 
+echo "Kraira se ${DIR} Direktorij"
+fi 
