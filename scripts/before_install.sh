3e84e63d3a1e214a93aa07626b1714f15c3fa218
Faruk Sinanovic <fsinanovic1@gmail.com>
2023-09-29 20:01:05 +0200

    edit

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # node i npm download
-curl -o https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash .~/.nvm/nvm.sh
+curl -o https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash . ~/.nvm/nvm.sh
 nvm install node
 
 #kreiranje foldera
